5e472bd6bcc1ce1b6e05be3c5bc36f72ac0915d7
alwaleed alhinai <alwaleed.h5505@gmail.com>
2025-03-17 11:37:56 +0400

    nest loop ,Print a Pyramid Pattern (For Loop)  and daimond shape

--- Program.cs
+++ Program.cs
@@ -377,24 +377,70 @@
             //    }
 
             //=====================  Print Pattern (For Loop)  ================================
-            int Number , i , j;
+            //int Number , i , j;
 
-            Console.WriteLine("Enter Number:");
-            Number = int.Parse(Console.ReadLine());
-            for (i =1; i<= Number; i++)
-            {
-              
-                for (j = 1; j <= i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
+            //Console.WriteLine("Enter Number:");
+            //Number = int.Parse(Console.ReadLine());
+            //for (i =1; i<= Number; i++)
+            //{
 
-            }
+            //    for (j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+            //=====================  Print a Pyramid Pattern (For Loop)   ================================
 
 
+            //int n;
+            //Console.WriteLine("Enter Number");
+            //n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //=====================  Print a Diamond Pattern(For Loop)  ================================
 
 
+            //int n;
+            //Console.WriteLine("Enter Number");
+            //n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
 
             //===================== Guess Game with helper ================================
 
